35c1387363620938f87556627ef3d20ca6962b91
Bivishan1 <sapkotabivishan@gmail.com>
2024-09-24 14:59:11 +0545

    random password generating feature added

---
+++ app.js
@@ -0,0 +1,44 @@
+const inputSlider = document.getElementById('inputSlider');
+const sliderValue = document.getElementById('sliderValue');
+const passBox = document.getElementById('passBox');
+
+const lowercaseE1 = document.getElementById('lowercase');
+const uppercaseE1 = document.getElementById('uppercase');
+const numbersE1 = document.getElementById('numbers');
+const symbolsE1 = document.getElementById('symbols');
+
+const generateBtn= document.getElementById('genBtn');
+const copyBtn = document.getElementById('copyIcon');
+const passIndicator = document.getElementById('passIndicator');
+
+
+const lowercaseLetters= 'abcdefghijklmnopqrstuvwxyz';
+const uppercaseLetters ='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const numbers='0123456789';
+const symbols="!@#$%^&*()_+-=[]{}\\|;':\",./<>?";
+
+sliderValue.textContent = inputSlider.value;
+
+inputSlider.addEventListener('input',()=> {
+	
+	sliderValue.textContent = inputSlider.value;
+	console.log(inputSlider.value);
+	generatePassword();
+});
+
+function generatePassword() {
+	const length = inputSlider.value;
+	let characters = '';
+	let password = '';
+
+	characters += lowercaseE1.checked ? lowercaseLetters : '';
+	characters += uppercaseE1.checked ? uppercaseLetters : "";
+	characters += numbersE1.checked ? numbers : "";
+	characters += symbolsE1.checked ? symbols : "";
+
+	for (let i =0;i < length;i++) {
+		password+= characters.charAt(Math.floor(Math.random() * characters.length));
+		console.log(password);
+	}
+	return passBox.value = password;
+}
